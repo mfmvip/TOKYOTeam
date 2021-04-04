fb800dd67c6012af265b99a5a2e948ad51ec9578
mfmvip <80322856+mfmvip@users.noreply.github.com>
2021-04-05 02:21:31 +0300

    Update TOKYO.lua

--- TOKYO.lua
+++ TOKYO.lua
@@ -3689,11 +3689,10 @@ local DevText = DevAbs:get(David.."DevText")
 if DevCh1 then DevCh = '\n✘︙*Dev Ch* ↬ ['..DevCh1..']' else DevCh = '' end
 tdcli_function({ID="GetUser",user_id_=DevId},function(arg,dp) 
 if dp.username_ ~= false then DevUser = '@'..dp.username_ else DevUser = dp.first_name_ end
-if dp.first_name_ ~= false then DevName = dp.first_name_ else DevName = dp.first_name_ end
-if DevText then
+if dp.first_name_ ~= false then DevGg = '['..dp.first_name_..'](tg://user?id='..DevId..')' endif DevText then
 Dev_Abs(msg.chat_id_, msg.id_, 1, DevText, 1, "md")
 else
-Dev_Abs(msg.chat_id_, msg.id_, 1, '✘︙*𝐍𝐀𝐌𝐄* ↬ ['..DevName..'](t.me/JZZZZ)\n✘︙*𝐔𝐒𝐄𝐑* ↬ ['..DevUser..']\n✘︙*𝐈𝐃* ↬ "..DevId..''..DevCh, 1, "md")
+Dev_Abs(msg.chat_id_, msg.id_, 1, '✘︙*𝐍𝐀𝐌𝐄* ↬ ['..DevGg..'](t.me/JZZZZ)\n✘︙*𝐔𝐒𝐄𝐑* ↬ ['..DevUser..']\n✘︙*𝐈𝐃* ↬ "..DevId..''..DevCh, 1, "md")
 end
 end,nil)
 end 
