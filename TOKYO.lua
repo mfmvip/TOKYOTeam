2af7e4e697b81cb5a1f20a72c1ce24e82f9b8417
mfmvip <80322856+mfmvip@users.noreply.github.com>
2021-04-04 20:33:54 +0300

    Update TOKYO.lua

--- TOKYO.lua
+++ TOKYO.lua
@@ -3689,10 +3689,11 @@ local DevText = DevAbs:get(David.."DevText")
 if DevCh1 then DevCh = '\n✘︙*Dev Ch* ↬ ['..DevCh1..']' else DevCh = '' end
 tdcli_function({ID="GetUser",user_id_=DevId},function(arg,dp) 
 if dp.username_ ~= false then DevUser = '@'..dp.username_ else DevUser = dp.first_name_ end
+if DevName = [dp.first_name_](tg://user?id='..result.id_..') end
 if DevText then
 Dev_Abs(msg.chat_id_, msg.id_, 1, DevText, 1, "md")
 else
-Dev_Abs(msg.chat_id_, msg.id_, 1, '✘︙*Dev User* ↬ ['..DevUser..']\n✘︙*Dev Id* ↬ '..DevId..''..DevCh, 1, "md")
+Dev_Abs(msg.chat_id_, msg.id_, 1, '✘︙*Dev User* ↬ ['..DevUser..']\n ✘︙*Dev name* ↬ ['..DevName..']\n✘︙*Dev Id* ↬ '..DevId..''..DevCh, 1, "md")
 end
 end,nil)
 end 
